44f74ec4ce9b943bb9133ed20c8d3e01399f9506
Piyapong Dangkham <95298381+piyapongdk@users.noreply.github.com>
2024-06-09 17:21:58 +0700

    Add files via upload

---
+++ lab02/element.js
@@ -0,0 +1,14 @@
+function more1(){
+	var m = document.getElementById("content1");
+	m.style.display = "none";
+}
+
+function more2(){
+	var m = document.getElementById("content2");
+	m.style.display = "none";
+}
+
+function more3(){
+	var m = document.getElementById("content3");
+	m.style.display = "none";
+}
