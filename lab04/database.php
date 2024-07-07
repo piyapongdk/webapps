aefc7444d6ccdc6552f7afcb8bf75f8b5a033f4f
Piyapong Dangkham <95298381+piyapongdk@users.noreply.github.com>
2024-07-07 17:19:07 +0700

    Add files via upload

---
+++ lab04/database.php
@@ -0,0 +1,13 @@
+<?php
+	$id = $_GET["staff"];
+	 
+    $conn = mysqli_connect("localhost","lab_sql","Lab@sql310","lab_sql");
+    mysqli_set_charset($conn, "utf8");
+    $sql = "SELECT * FROM employees WHERE employeeNumber = ".$id;
+	echo("SQL :".$sql);
+	echo("<br><br>");
+	
+    $result = mysqli_query($conn,$sql);
+    $row = mysqli_fetch_assoc($result);
+	echo($row["employeeNumber"]." ".$row["firstName"]." ".$row["lastName"]);
+?>
